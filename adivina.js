0e78e47946fefaf9fb30b2b5017e0299ee0fafbd
01Yaros <129272759+01Yaros@users.noreply.github.com>
2023-05-10 20:30:59 +0200

    Update adivina.js

--- adivina.js
+++ adivina.js
@@ -1,13 +1,13 @@
-// genera el numero 1-10
+// genera el número 1-10
 var number = Math.floor(Math.random() * 10) + 1;
-// comprova si es correcte el numero
+// comprova si és correcte el numero
 function checkGuess() {
   var guess = document.getElementById("guessInput").value;
   if (guess == number) {
-    alert("¡Adivinaste!");
+    alert("FELIÇITATS, HAS ENDEVINAT EL NÚMERO! TORNEM-HI?");
   } else if (guess < number) {
-    alert("masa petit");
+    alert("El número és massa PETIT");
   } else {
-    alert("masa gran");
+    alert("El número és massa GRAN");
   }
 }
